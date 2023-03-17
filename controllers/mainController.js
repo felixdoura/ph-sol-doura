50491ddba8ec831b62e47141954fa9047b60ce41
Felix <felixdoura@hotmail.com>
2023-03-17 14:45:27 -0300

    Muchisimos cambios, incluida la instalacion de yarn

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,20 +1,64 @@
-let products = [
+let productos = [
 
     {
         id: 1,
+        nombre: "foto01",
         descripcion: "imagen001",
         precio: 1000,
         descuento: 10,
+        imagen: "/images/PortfolioSol-Moda0447.jpg"
+    },
 
-    }
+    {
+        id: 2,
+        nombre: "foto02",
+        descripcion: "imagen002",
+        precio: 1500,
+        descuento: 10,
+        imagen: "/images/soldoura_amatista_2021.jpg"
+    },
+
+    {
+        id: 3,
+        nombre: "foto03",
+        descripcion: "imagen002",
+        precio: 1500,
+        descuento: 10,
+        imagen: "/images/SolDoura-Estetica de lo cotidiano.jpg"
+    },
+
+    {
+        id: 4,
+        nombre: "foto04",
+        descripcion: "imagen001",
+        precio: 1000,
+        descuento: 10,
+        imagen: "/images/14.jpg"
+    },
+
+    {
+        id: 5,
+        nombre: "foto05",
+        descripcion: "imagen002",
+        precio: 1500,
+        descuento: 10,
+        imagen: "/images/La-Reserva-IG-6.jpg"
+    },
+
+    {
+        id: 6,
+        nombre: "foto06",
+        descripcion: "imagen002",
+        precio: 1500,
+        descuento: 10,
+        imagen: "/images/Porfolio-Sol-Rockearte-32.jpg"
+    },
 ]
 
 
 module.exports = {
-    index : (req, res) => {
-       
-        res.render("home");
-
+    home : (req, res) => {
+        res.render("home", {productos});
     },
 
 }
--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -2,13 +2,11 @@ const express = require("express");
 const router = express.Router();
 const mainController = require("../controllers/mainController");
 
-router.get ("/", mainController.index);
+router.get ("/", mainController.home);
 
 // router.get ("/register", mainController.register);
 
 // router.get (/login, mainController.login);
-
-
 
 
 
